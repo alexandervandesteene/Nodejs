8c93b09f02b3f20d88d8c6ad528a111f40a89514
alexandervandesteene <alexandervandesteene@mbpvanalexander.home>
2015-11-02 20:18:44 +0100

    back back

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-var express = require('express');
-var app = express();
-
-var mysql = require("mysql");
-var config = require("./config.json");
-
-var bodyParser = require('body-parser');
-
-app.set('view engine', 'ejs');
-app.use(bodyParser());
-
 
 var http = require('http')
 var port = process.env.PORT || 1337;
+
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
