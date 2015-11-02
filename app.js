10878d1453b7d001e31ad997fb77e168e3578fd0
alexandervandesteene <alexandervandesteene@mbpvanalexander.home>
2015-11-02 19:48:52 +0100

    stap 2

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+var express = require('express');
+var app = express();
+
+var mysql = require("mysql");
+var config = require("./config.json");
+
+var bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.use(bodyParser());
+
+
 var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
